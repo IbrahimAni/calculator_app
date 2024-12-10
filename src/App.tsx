cc7a17b1541fee45250953152624f74d609f41e7
Ibrahim Anifowoshe <ibrahim.anifowoshe.uk@gmail.com>
2024-12-10 22:10:31 +0000

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from 'react';
+import Calculator from './pages/calculator';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
